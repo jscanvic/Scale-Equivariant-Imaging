fe393347464beeefcde7dde7299b556015927fd0
Jérémy Scanvic <jscanvicz@gmail.com>
2024-07-06 20:11:40 +0200

    factor things out

--- src/transforms.py
+++ src/transforms.py
@@ -11,64 +11,83 @@ def sample_from(values, shape=(1,), dtype=torch.float32, device="cpu"):
     return values[indices]
 
 
+def sample_scaling_parameters(image_count, device, dtype):
+    # Sample a random scale factor for each batch element
+    factor = sample_from([0.75, 0.5], shape=(image_count,), device=device)
+
+    # Sample a random transformation center for each batch element
+    # with coordinates in [-1, 1]
+    center = torch.rand((image_count, 2), dtype=dtype, device=device)
+    center = center.view(image_count, 1, 1, 2)
+    center = 2 * center - 1
+
+    return factor, center
+
+
+def padded_scaling_transform(x, factor, center, mode, padding_mode, antialiased):
+    b, _, h, w = x.shape
+
+    # Compute the sampling grid for the scale transformation
+    u = torch.arange(w, dtype=x.dtype, device=x.device)
+    v = torch.arange(h, dtype=x.dtype, device=x.device)
+    u = 2 / w * u - 1
+    v = 2 / h * v - 1
+    U, V = torch.meshgrid(u, v)
+    grid = torch.stack([V, U], dim=-1)
+    grid = grid.view(1, h, w, 2).repeat(b, 1, 1, 1)
+    grid = 1 / factor.view(b, 1, 1, 1).expand_as(grid) * (grid - center) + center
+
+    if not antialiased:
+        x = F.grid_sample(
+            x,
+            grid,
+            mode=mode,
+            padding_mode=padding_mode,
+            align_corners=True,
+        )
+    else:
+        xs = []
+        for i in range(x.shape[0]):
+            # Apply the anti-aliasing filter
+            z = F.interpolate(
+                x[i : i + 1, :, :, :],
+                scale_factor=factor[i].item(),
+                mode=mode,
+                antialias=True,
+            )
+            z = F.grid_sample(
+                z,
+                grid[i : i + 1],
+                mode=mode,
+                padding_mode=padding_mode,
+                align_corners=True,
+            )
+            z = z.squeeze(0)
+            xs.append(z)
+        x = torch.stack(xs)
+    return x
+
+
 class PaddedScalingTransform(Module):
     def __init__(self, antialias=False):
         super().__init__()
-
-        self.padding_mode = "reflection"
-        self.mode = "bicubic"
         self.antialias = antialias
 
     def forward(self, x):
-        b, _, h, w = x.shape
-
-        # Sample a random scale factor for each batch element
-        factor = sample_from([.75, .5], shape=(b,), device=x.device)
-
-        # Sample a random transformation center for each batch element
-        # with coordinates in [-1, 1]
-        center = torch.rand((b, 2), dtype=x.dtype, device=x.device)
-        center = center.view(b, 1, 1, 2)
-        center = 2 * center - 1
-
-        # Compute the sampling grid for the scale transformation
-        u = torch.arange(w, dtype=x.dtype, device=x.device)
-        v = torch.arange(h, dtype=x.dtype, device=x.device)
-        u = 2 / w * u - 1
-        v = 2 / h * v - 1
-        U, V = torch.meshgrid(u, v)
-        grid = torch.stack([V, U], dim=-1)
-        grid = grid.view(1, h, w, 2).repeat(b, 1, 1, 1)
-        grid = 1 / factor.view(b, 1, 1, 1).expand_as(grid) * (grid - center) + center
-
-        if self.antialias:
-            xs = []
-            for i in range(x.shape[0]):
-                # Apply the anti-aliasing filter
-                z = F.interpolate(
-                    x[i : i + 1, :, :, :],
-                    scale_factor=factor[i].item(),
-                    mode=self.mode,
-                    antialias=True,
-                )
-                z = F.grid_sample(
-                    z,
-                    grid[i : i + 1],
-                    mode=self.mode,
-                    padding_mode=self.padding_mode,
-                    align_corners=True,
-                )
-                z = z.squeeze(0)
-                xs.append(z)
-            x = torch.stack(xs)
-        else:
-            x = F.grid_sample(
-                x,
-                grid,
-                mode=self.mode,
-                padding_mode=self.padding_mode,
-                align_corners=True,
+        factor, center = sample_scaling_parameters(
+                image_count=x.shape[0],
+                device=x.device,
+                dtype=x.dtype,
             )
+
+        x = padded_scaling_transform(
+            x,
+            factor=factor,
+            center=center,
+            antialiased=self.antialias,
+            mode="bicubic",
+            padding_mode="reflection",
+        )
 
         return x
 
