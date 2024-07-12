ec29f4763f881b2ace8d7c4d682e2669e30b5a8a
Jérémy Scanvic <jscanvicz@gmail.com>
2024-07-12 22:43:04 +0200

    move a class to a new file

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,88 +1,11 @@
-import torch
 from torch.nn import Module
 from torch.utils.data import Dataset as BaseDataset
-from torchvision.transforms import InterpolationMode, functional as TF
-from functools import wraps
+from torchvision.transforms import functional as TF
 
 from noise2inverse import Noise2InverseTransform
+from .ground_truth import GroundTruthDataset
 from .crop import CropPair
-from .div2k import Div2K
-from .tomography import TomographyDataset
-from .urban100 import Urban100
 from .single_image import SingleImageDataset
-
-
-class GroundTruthDataset(BaseDataset):
-    def __init__(
-        self,
-        blueprint,
-        datasets_dir,
-        dataset_name,
-        split,
-        download,
-        size,
-        memoize_gt,
-    ):
-        super().__init__()
-        self.size = size
-        self.memoize_gt = memoize_gt
-
-        if dataset_name == "div2k":
-            self.dataset = Div2K(split, datasets_dir, download=download)
-        elif dataset_name == "urban100":
-            self.dataset = Urban100(split, datasets_dir, download=download)
-        elif dataset_name == "ct":
-            self.dataset = TomographyDataset(split, datasets_dir, download=download)
-        elif dataset_name == "single_image":
-            self.dataset = SingleImageDataset(**blueprint[SingleImageDataset.__name__])
-        else:
-            raise ValueError(f"Unknown dataset: {dataset_name}")
-
-    def get_unique_id(self, index):
-        if hasattr(self.dataset, "get_unique_id"):
-            id = self.dataset.get_unique_id(index)
-        else:
-            id = index
-        return id
-
-    @staticmethod
-    def memoize_load_image(f):
-        cache = {}
-
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            self = args[0]
-            if not self.memoize_gt:
-                x = f(*args, **kwargs)
-            else:
-                key = (args, frozenset(kwargs.items()))
-                if key not in cache:
-                    x = f(*args, **kwargs)
-                    device = x.device
-                    # NOTE: This should be unnecessary.
-                    x = x.to("cpu")
-                    cache[key] = (device, x)
-                device, x = cache[key]
-                x = x.to(device)
-            return x
-
-        return wrapper
-
-    @memoize_load_image
-    def __getitem__(self, index):
-        x = self.dataset[index]
-        if self.size is not None:
-            x = TF.resize(
-                x,
-                size=self.size,
-                interpolation=InterpolationMode.BICUBIC,
-                antialias=True,
-            )
-
-        return x
-
-    def __len__(self):
-        return len(self.dataset)
 
 
 class SyntheticDataset(BaseDataset):
