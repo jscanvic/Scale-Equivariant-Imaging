4effea9061dca42602002bb276cd12c0232b7ccf
Jérémy Scanvic <jscanvicz@gmail.com>
2024-09-11 13:18:22 +0200

    add the weights distance loss

---
+++ src/losses/weights_distance_loss.py
@@ -0,0 +1,29 @@
+import torch
+from torch.nn.functional import mse_loss
+
+from copy import deepcopy
+
+class WeightsDistanceLoss:
+    def __init__(self, pretrained_model, lambd, device):
+        pretrained_model = deepcopy(pretrained_model)
+        pretrained_weights = pretrained_model.named_parameters()
+        pretrained_weights = dict(pretrained_weights)
+        self.pretrained_weights = pretrained_weights
+        self.lambd = lambd
+        self.device = device
+
+    def __call__(self, model):
+        keys = self.pretrained_weights.keys()
+        keys = set(keys)
+
+        weights = model.named_parameters()
+        weights = dict(weights)
+
+        assert keys == set(weights.keys())
+
+        loss = torch.zeros(1, device=self.device)
+        for key in keys:
+            pretrained_param = self.pretrained_weights[key]
+            param = weights[key]
+            loss += mse_loss(pretrained_param, param)
+        return self.lambd * loss / len(keys)
